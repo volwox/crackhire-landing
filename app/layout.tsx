c0ce25bd5011bfb0eba30db89ac4246d07d366cd
volwox <contact@nexusglobalops.com>
2026-01-22 16:11:13 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import Script from 'next/script';
 import './globals.css';
 import { SITE_CONFIG } from '@/lib/constants';
 
+const GA_MEASUREMENT_ID = 'G-BVQ3CYFHPH';
+const TIKTOK_PIXEL_ID = 'D5P22C3C77U93QK9T2TG';
+
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_CONFIG.url),
   title: {
@@ -58,20 +61,66 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
+        {/* ========================= */}
+        {/* Google Analytics (GA4) */}
+        {/* ========================= */}
         <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-BVQ3CYFHPH"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
         />
-        <Script id="ga-init" strategy="afterInteractive">
+        <Script id="ga4-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-BVQ3CYFHPH', {
+            gtag('config', '${GA_MEASUREMENT_ID}', {
               anonymize_ip: true,
-              send_page_view: true
             });
+          `}
+        </Script>
+
+        {/* ========================= */}
+        {/* TikTok Pixel */}
+        {/* ========================= */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject = t;
+              var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+              ttq.setAndDefer = function(t, e) {
+                t[e] = function() {
+                  t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+              };
+              for (var i = 0; i < ttq.methods.length; i++) {
+                ttq.setAndDefer(ttq, ttq.methods[i]);
+              }
+              ttq.instance = function(t) {
+                for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) {
+                  ttq.setAndDefer(e, ttq.methods[n]);
+                }
+                return e;
+              };
+              ttq.load = function(e, n) {
+                var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
+                ttq._i = ttq._i || {};
+                ttq._i[e] = [];
+                ttq._i[e]._u = r;
+                ttq._t = ttq._t || {};
+                ttq._t[e] = +new Date;
+                ttq._o = ttq._o || {};
+                ttq._o[e] = n || {};
+                var o = document.createElement("script");
+                o.type = "text/javascript";
+                o.async = true;
+                o.src = r + "?sdkid=" + e + "&lib=" + t;
+                var a = document.getElementsByTagName("script")[0];
+                a.parentNode.insertBefore(o, a);
+              };
+              ttq.load('${TIKTOK_PIXEL_ID}');
+              ttq.page();
+            }(window, document, 'ttq');
           `}
         </Script>
       </head>
